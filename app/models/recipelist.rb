219b2db25160cb8f23ed7ba60448230f928c0d44
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-02-19 15:09:12 +0900

    add-recipelist2

--- app/models/recipelist.rb
+++ app/models/recipelist.rb
@@ -1,2 +1,7 @@
 class Recipelist < ApplicationRecord
+  belongs_to :recipe
+
+  mount_uploader :image, ImageUploader
+
+
 end
--- db/migrate/20200218104555_create_recipelists.rb
+++ db/migrate/20200218104555_create_recipelists.rb
@@ -1,7 +1,9 @@
 class CreateRecipelists < ActiveRecord::Migration[5.2]
   def change
     create_table :recipelists do |t|
-
+      t.string :text 
+      t.string :image
+      t.references :recipe, foreign_key: true
       t.timestamps
     end
   end
