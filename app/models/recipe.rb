9f08aae26f64146b9ab3fc5f0fe49608881dcb76
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-02-27 15:29:43 +0900

    fix-destroy

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,16 +2,16 @@ class Recipe < ApplicationRecord
   validates :title, :time, presence: true
 
   belongs_to :user
-  has_many :recipelists
-  has_many :ingredients
-  has_many :likes
+  has_many :recipelists, dependent: :destroy
+  has_many :ingredients, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   has_many :calculations
   has_many :calculations_users, through: :calculations, source: :user
 
-  accepts_nested_attributes_for :recipelists
-  accepts_nested_attributes_for :ingredients
+  accepts_nested_attributes_for :recipelists,  allow_destroy: :true
+  accepts_nested_attributes_for :ingredients,  allow_destroy: :true
 
   mount_uploader :image, ImageUploader
 
