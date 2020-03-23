f7db5ddaf48bdffeb4ad6186a788fc34ec8ef7e1
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-03-23 19:49:02 +0900

    error_deploy_fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.147.81', user: 'ec2-user', roles: %w{app db web}
+server '13.115.121.255', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
