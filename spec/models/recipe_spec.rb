a38a5597dbc314d767fbb8d5dfa561aa6dd0f4bf
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-03-29 12:06:50 +0900

    recipe_test2

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
-describe Recipe do
-  describe "#create" do
-    it "is invalid without a title" do
-      recipe = Recipe.new(title: "", image: "オムライス.jpg", time: "15",
-      serving: "3", worktime: "13")
-      recipe.valid?
-      expect(recipe.errors[:title]).to include("can't be blank")
-    end
-  end
-end
+# require 'rails_helper'
+# describe Recipe do
+#   describe "#create" do
+#     it "is invalid without a title" do
+#       recipe = Recipe.new(title: "", image: "オムライス.jpg", time: "15",
+#       serving: "3", worktime: "13")
+#       recipe.valid?
+#       expect(recipe.errors[:title]).to include("can't be blank")
+#     end
+#   end
+# end
