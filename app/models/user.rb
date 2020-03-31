e80035cdb2e621cda22050ca83112ad01d5349fb
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-03-31 17:53:36 +0900

    add_google_regist

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :recipes, dependent: :destroy
   has_many :likes,dependent: :destroy
@@ -10,11 +10,29 @@ class User < ApplicationRecord
 
   has_many :calculations
   has_many :calculations_recipes, through: :calculations, source: :recipe
+  has_many :sns_credentials
 
   validates :nickname, presence: true, length: { maximum: 20 }
 
+
   def already_liked?(recipe)
     self.likes.exists?(recipe_id: recipe.id)
+  end
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
 
 end
